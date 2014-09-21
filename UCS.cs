7df847c39215e6ba9990e18188a7459ecbb0ae7f
Mark Skelton <mark@software13.co.uk>
2014-09-21 17:04:25 +0100

    Initial version

---
+++ UCS.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * The Universal Coordinate System provides a simple class to convert screen independent co-ordinates from -1.0 to 1.0 into screen space,
+ * taking into account aspect ratio.
+ * It's a bit raw in it's current state, so improvements are welcome.
+ */
+public class UCS : MonoBehaviour {
+	static float aspectX = 0.0f;
+	static float aspectY = 0.0f;
+	static float halfX = Screen.width / 2.0f;
+	static float halfY = Screen.height / 2.0f;
+	static float padX = 0.0f;
+	static float padY = 0.0f;
+
+
+	public static Rect ConvertRect(Rect rect) {
+		rect.x = ConvertX (rect.x);
+		rect.y = ConvertY (rect.y);
+		rect.width = ConvertW (rect.width);
+		rect.height = ConvertH (rect.height);
+		return rect;
+	}
+
+	public static float ConvertX(float x) {
+		return ConvertW (x+GetPadX()) + (halfX / GetAspectX ());
+	}
+
+	public static float ConvertY(float y) {
+		return ConvertH (y+GetPadY()) + (halfY / GetAspectY ());
+	}
+
+	/**
+	 * Return additional screen space on X axis
+	 */
+	public static float GetPadX() {
+		CheckAspect ();
+		return padX;
+	}
+
+	public static float GetPadY() {
+		CheckAspect ();
+		return padY;
+	}
+
+	public static float ConvertW(float x) {
+		return (x * (halfX / GetAspectX()));
+	}
+	
+	public static float ConvertH(float y) {
+		return (y * (halfY / GetAspectY()));
+	}
+
+	private static float GetAspectY() {
+		CheckAspect ();
+		return aspectY;
+	}
+	private static float GetAspectX() {
+		CheckAspect ();
+		return aspectX;
+	}
+
+	private static void CheckAspect() {
+		if (aspectX != 0.0f)
+			return;
+		aspectX = 1.0f; aspectY = 1.0f;
+		if(Screen.width > Screen.height) {
+			aspectX = (Screen.width * 1.0f) / (Screen.height * 1.0f);
+		} else {
+			aspectY = (Screen.height * 1.0f) / (Screen.width * 1.0f);
+		}
+		padX = aspectX - 1.0f;
+		padY = aspectY - 1.0f;
+	}
+
+	/**
+	 * Returns the full available width of the screen in universal co-ordinates.
+	 */
+	private static float GetWidth() {
+		CheckAspect ();
+		return 2.0f + (padX * 2);
+	}
+
+	/**
+	 * Returns the full available width of the screen in universal co-ordinates.
+	 */
+	private static float GetHeight() {
+		CheckAspect ();
+		return 2.0f + (padY * 2);
+	}
+}
---
+++ UCSTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UCSTest : MonoBehaviour {
+	void Start () {
+	
+	}
+	
+	void Update () {
+	}
+
+	void OnGUI() {
+		Debug.Log (UCS.ConvertRect (new Rect (-1.0f, -1.0f, 2.0f, 2.0f)));
+		GUI.Box (UCS.ConvertRect(new Rect(-1.0f, -1.0f, 2.0f, 2.0f)), "Test");
+	}
+}
